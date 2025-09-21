dca2c4b97805aa1cbcba8713795255edcb2b2fc4
Stepan <stepsyota@gmail.com>
2025-09-21 21:56:43 +0300

    Complete: first task

--- Lab3/main.asm
+++ Lab3/main.asm
@@ -1,8 +1,9 @@
 .DATA
 	; First ex
-	array qword 1, 4, 6, 13, 2, 8, 14
-	array_el_size = TYPE array
-	count = ($ - array) / TYPE array
+	first_array qword 1, 4, 6, 13, 2, 8, 14
+	first_el_size = TYPE first_array
+	first_count = ($ - first_array) / TYPE first_array
+	first_summa dword ?
 
 .CODE
 	main PROC
@@ -10,23 +11,43 @@
 	; First ex
 	; Найти сумму 3 наибольших элементов массива.
 
-		mov rcx, Count
+		mov eax, first_count ; Если размер массива < 3 элементов
+		cmp eax, 3
+		jl SKIP_FIRST
+
+		mov rcx, first_count
 		dec rcx
 	L1: 
 		push rcx ; сохранить счетчик внешнего цикла
-		lea rsi, array ; ESI указывает на первый элемент
+		lea rsi, first_array ; ESI указывает на первый элемент
 	L2: 
 		mov eax, [rsi] ; взять число из массива
-		cmp [rsi+4], eax ; сравнить со следующим
+		cmp [rsi+first_el_size], eax ; сравнить со следующим
 		jg L3 ; if ([ESI+4] > [ESI]) не обменивать
-		xchg eax,[rsi+4] ; обменять значения соседних
+		xchg eax,[rsi+first_el_size] ; обменять значения соседних
 		mov [rsi], eax ; элементов массива
 	L3: 
-		add rsi,4 ; cдвинуть указатель на следующий элемент
+		add rsi,first_el_size ; cдвинуть указатель на следующий элемент
 		loop L2 ; внутренний цикл
 		pop rcx ; восстановить счетчик внешнего цикла
 		loop L1 ; повторить внешний цикл
 	L4:
+		lea rsi, first_array
+		mov rax, first_el_size
+		mov rbx, first_count
+		dec rbx
+		mul rbx
+		add rsi, rax ; Получил последний элемент массива
+		
+		xor ecx, ecx
+		add ecx, [rsi]
+		sub rsi, first_el_size
+		add ecx, [rsi]
+		sub rsi, first_el_size
+		add ecx, [rsi]
+		mov first_summa, ecx
+
+	SKIP_FIRST:
 		ret
 	main ENDP
 END
