69a99dee715ec12031fa45d569d181e89903c5f5
Stepan <stepsyota@gmail.com>
2025-10-20 18:43:53 +0300

    5.3 start

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -9,6 +9,15 @@ extern "C" {
     double avx_cosh(double x, double eps = 1e-6);
 }
 
+double poly(double x, int n = 5) {
+    double res = 0;
+    res += 10;
+    for (int k = 1; k <= n; ++k) {
+        double sigma = pow(-1, k + 1) * (2 * k - 1) * pow(x, 2 * k - 1);
+        res += sigma;
+    }
+    return res;
+}
 int main() {
     double x = 1.1;
     double eps = 1e-3;
@@ -16,6 +25,8 @@ int main() {
     cout << "0. cosh(x) by math.h = \t" << cosh(x) << endl;
     cout << "1. cosh(x) by SSE    = \t" << sse_cosh(x, eps) << endl;
     cout << "2. cosh(x) by AVX    = \t" << avx_cosh(x, eps) << endl;
+    cout << "3. polynomial by math.h    = \t" << poly(1) << endl;
+    cout << "3. polynomial by FMA       = \t" << fma_poly(1) << endl;
 
     return 0;
 }
